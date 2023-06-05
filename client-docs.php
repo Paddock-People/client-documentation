5204cbff2be2079592310768f823b11ea22aef71
kyle-lambAgency <kyle@lambagency.com.au>
2023-06-05 12:15:35 +1000

    first commit

---
+++ client-docs.php
@@ -0,0 +1,135 @@
+<?php
+
+// Get the header file of the active theme
+$plugin_folder_url = plugins_url('/', __FILE__);
+
+include('page-templates/header.php');
+?>
+
+<link rel="stylesheet" href="<?php echo $plugin_folder_url; ?>styles.css">
+
+<div class="atlas-docs">
+  <div class="container">
+
+    <div class="row">
+      <div class="col-md-4 col-lg-3 pb-20 list-border">
+        <h4 class="mb-20">Help Articles</h4>
+        <div id="atlas-docs"></div>
+        <div id="no-results" style="display: none;">No results found.</div>
+      </div>
+      <div class="col-md-8 col-lg-9 mt-md-0 mt-30">
+        <div id="post-content">
+          <h2 class="mb-0">ATLAS DOCS</h2>
+          <p>Brought to you by the Paddock People</p>
+        </div>
+      </div>
+    </div>
+
+    <script>
+      document.addEventListener("DOMContentLoaded", function() {
+        let searchInput = document.getElementById("search-input");
+        let postList = document.getElementById("atlas-docs");
+        let noResults = document.getElementById("no-results");
+        let allPosts = [];
+
+        fetch("http://apollo.local/wp-json/wp/v2/lamb-documentation?per_page=100")
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(posts) {
+            posts.sort((a, b) => a.name < b.name ? -1 : a.name === b.name ? 0 : 1)
+            allPosts = posts;
+            renderPosts(allPosts);
+          });
+
+        function renderPosts(posts) {
+          postList.innerHTML = '';
+          noResults.style.display = "none";
+
+          if (posts.length === 0) {
+            noResults.style.display = "block";
+          } else {
+            posts.forEach(function(post) {
+              let postLink = document.createElement("a");
+              postLink.href = "#";
+              postLink.className = "post-link";
+              postLink.dataset.postId = post.id;
+              postLink.innerText = post.title.rendered;
+              postLink.addEventListener("click", function(e) {
+                e.preventDefault();
+                let postId = e.target.dataset.postId;
+                let postContent = document.getElementById("post-content");
+                fetch("http://apollo.local/wp-json/wp/v2/lamb-documentation/" + postId)
+                  .then(function(response) {
+                    return response.json();
+                  })
+                  .then(function(post) {
+                    postContent.innerHTML = post.content.rendered;
+
+                    // Update the URL
+                    let postTitle = post.title.rendered.replace(/\s+/g, "-")
+                      .toLowerCase(); // Replace spaces with hyphens
+                    let newUrl = window.location.href.split("#")[0] + "#" +
+                      postTitle; // Update the URL with the post title
+                    history.pushState({}, "", newUrl); // Update the URL without reloading the page
+                  });
+              });
+              postList.appendChild(postLink);
+            });
+          }
+        }
+
+        // Add event listener for hashchange event
+        window.addEventListener("hashchange", handleHashChange);
+
+        // Call handleHashChange initially to load the post content if the URL has a hash fragment
+        handleHashChange();
+
+        function handleHashChange() {
+          var postTitle = getPostTitleFromUrl();
+          if (postTitle) {
+            fetchPostContent(postTitle);
+          } else {
+            // Handle the case when there is no post title in the URL
+            // You can display a default content or show an error message
+            // For example, postContent.innerHTML = 'No post selected.';
+          }
+        }
+
+        function getPostTitleFromUrl() {
+          var hash = window.location.hash;
+          if (hash) {
+            // Remove the leading "#" character and any leading/trailing whitespaces
+            var postTitle = hash.substr(1).trim();
+            return postTitle;
+          }
+          return null;
+        }
+
+        function fetchPostContent(postTitle) {
+          var postContent = document.getElementById("post-content");
+          fetch("http://apollo.local/wp-json/wp/v2/lamb-documentation?slug=" + encodeURIComponent(postTitle))
+            .then(function(response) {
+              return response.json();
+            })
+            .then(function(posts) {
+              if (posts.length > 0) {
+                postContent.innerHTML = posts[0].content.rendered;
+              }
+            });
+        }
+
+
+        searchInput.addEventListener("input", function() {
+          let searchTerm = searchInput.value.toLowerCase();
+          let filteredPosts = allPosts.filter(function(post) {
+            let title = post.title.rendered.toLowerCase();
+            return title.includes(searchTerm);
+          });
+          renderPosts(filteredPosts);
+        });
+      });
+    </script>
+
+  </div>
+</div>
---
+++ lamb-documentation.php
@@ -0,0 +1,186 @@
+<?php
+
+/**
+ * Atlas Documenation
+ *
+ * @package       ATLASDOCS
+ * @author        Lamb Agency
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   Atlas Documenation
+ * Plugin URI:    https://www.lambagency.com.au/
+ * Description:   This plugin creates a local version of documentation on the client site.
+ * Version:       1.0.0
+ * Author:        Lamb Agency
+ * Author URI:    https://www.lambagency.com.au/
+ * Text Domain:   lamb-documentation
+ * Domain Path:   /languages
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) exit;
+
+
+function add_lamb_docs_page()
+{
+  add_menu_page(
+    'Lamb Docs', // Page title
+    'Lamb Docs', // Menu title
+    'manage_options', // Required capability to access the page
+    'lamb-docs', // Page slug
+    'lamb_documentation_page_contents', // Callback function to display the page content
+    'dashicons-book-alt', // Menu icon
+    100 // Menu position
+  );
+}
+add_action('admin_menu', 'add_lamb_docs_page');
+
+// add_action('admin_head', 'view_only');
+
+// function view_only()
+// {
+//   echo '<style>
+//     .post-type-lamb-documentation .row-actions span:not(.view) {
+//       display:none;
+//     } 
+//   </style>';
+// }
+
+function lamb_documentation_page_contents()
+{
+?>
+  <h1>
+    Lamb Documentation.
+  </h1>
+
+  <?php
+
+  // General check for user permissions.
+  if (!current_user_can('manage_options')) {
+    wp_die(__('You do not have sufficient privileges to access this page.'));
+  }
+
+  // Start building the page
+
+  echo '<div class="wrap">';
+  echo '<form action="admin.php?page=lamb-docs" method="post">';
+
+  // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
+  wp_nonce_field('rest_documentation_data');
+  echo '<input type="hidden" value="true" name="import_documentation" />';
+  echo 'Enter Site ID:';
+  echo '<br/><br/>';
+  echo '<input type="number" value="true" name="documentation_cat_id" />';
+  submit_button('Import');
+  echo '</form>';
+
+  // Check whether the button has been pressed AND also check the nonce
+  if (isset($_POST['import_documentation']) && check_admin_referer('rest_documentation_data')) {
+    // the button has been pressed AND we've passed the security check
+    //echo $_POST['documentation_cat_id'];
+    import_docs($_POST['documentation_cat_id']);
+  }
+
+  echo '</div>';
+
+  ?>
+
+<?php
+}
+
+function import_docs($cat_it)
+{
+
+  if (!$cat_it) {
+    echo '<h4> Please enter your Site ID </h4>';
+  } else {
+
+    // Delete all existing posts
+    $args = array(
+      'post_type'      => 'lamb-documentation',
+      'posts_per_page' => -1,
+      'post_status'    => 'any',
+    );
+    $existing_posts = get_posts($args);
+
+    foreach ($existing_posts as $post) {
+      wp_delete_post($post->ID, true);
+    }
+
+    $json_url = 'https://atlas.paddockpeople.com.au/wp-json/wp/v2/client-documentation?client=' . $cat_it . '&per_page=100';
+
+    $json = file_get_contents($json_url);
+    $lambDocumantation = json_decode($json);
+
+    if ($lambDocumantation) {
+      echo '<h4 style="margin-bottom:10;">Articles Imported:</h4>';
+      foreach ($lambDocumantation as $item) {
+        // $full_image_urls  = str_replace("/wp-content/uploads/", "https://www.apollopatios.com.au/wp-content/uploads/", $item->content->rendered);
+
+        $my_post = array(
+          'post_title'    => wp_strip_all_tags($item->title->rendered),
+          'post_content'  => $item->content->rendered,
+          'post_status'   => 'publish',
+          'post_type'     => 'lamb-documentation',
+        );
+
+        // Insert the post into the database
+        wp_insert_post($my_post);
+
+        echo $item->title->rendered . ' imported';
+        echo '<br/>';
+      }
+    } else {
+      echo '<h4> Site ID does not exist. </h4>';
+    }
+  }
+}
+
+// Register the rewrite rule
+function my_plugin_register_rewrite_rule($rules)
+{
+  $new_rules = array(
+    'atlas-docs/?$' => 'index.php'
+  );
+
+  return $new_rules + $rules;
+}
+add_filter('rewrite_rules_array', 'my_plugin_register_rewrite_rule');
+
+// Register the query variable
+function my_plugin_register_query_var($vars)
+{
+  $vars[] = 'my_plugin_page';
+  return $vars;
+}
+add_filter('query_vars', 'my_plugin_register_query_var');
+
+// Hook into the template_include filter to handle the rendering of your custom template
+function my_plugin_template_include($template)
+{
+  // Check if the custom query variable is set
+  if (get_query_var('my_plugin_page')) {
+    // Set the path to your custom template file
+    $template = plugin_dir_path(__FILE__) . 'client-docs.php';
+  }
+
+  return $template;
+}
+add_filter('template_include', 'my_plugin_template_include');
+
+require 'plugin-update-checker/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+  'https://github.com/kyle-lambAgency/lamb-documentation/',
+  __FILE__,
+  'lamb-documentation'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
